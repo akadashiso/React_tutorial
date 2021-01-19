5900485f2ec5dc0c8423c3c6790e67135c2d9b36
akadashiso <akadashiso@gmail.com>
2021-01-19 22:07:51 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+import ReactDom from 'react-dom'
+
+function Greeting() {
+return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
+}
+
+const Person = () => <h2>john doe</h2>;
+const Message = () => {
+  return <p>this is my message</p>;
+
+};
+ReactDom.render(<Greeting />, document.getElementById('root'));

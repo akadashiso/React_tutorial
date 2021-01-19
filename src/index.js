9319c29437c61b19f11ee66e420178cf676cc9c6
akadashiso <akadashiso@gmail.com>
2021-01-19 22:40:35 +0900

    Added a Booklist component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 import React, { Fragment } from 'react'
 import ReactDom from 'react-dom'
 
-function Greeting() {
-return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+function BookList() {
+  return (
+  <section>
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+  </section>
   );
 }
 
-const Person = () => <h2>john doe</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
-
+const Book = () =>{
+  return (
+  <article>
+    <Image></Image>
+    <Title />
+    <Author />
+  </article>
+  );
 };
-ReactDom.render(<Greeting />, document.getElementById('root'));
+const Image = () => (
+<img
+ src="https://images-na.ssl-images-amazon.com/images/I/91p5b0UgbKL._AC_UL200_SR200,200_.jpg" 
+ alt=""
+ />
+);
+
+const Title = () => <h1>The Four Agreements: A Practical Guide to Personal Freedom</h1>
+const Author = () => <h4>Don Miguel Ruiz</h4>
+
+ReactDom.render(<BookList />, document.getElementById('root')
+);

5372b0ce56a9d333912d9bd9a8eed3a202d58288
akadashiso <akadashiso@gmail.com>
2021-01-20 22:44:02 +0900

    Added a names value

--- src/index.js
+++ src/index.js
@@ -4,26 +4,36 @@ import ReactDom from 'react-dom'
 // CSS
 import './index.css';
 //  setup vars
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/91p5b0UgbKL._AC_UL200_SR200,200_.jpg',
-  title: 'A Practical Guide to Personal Freedom',
-  author: 'Don Miguel Ruiz'
-}
-
+const books = [
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/91p5b0UgbKL._AC_UL200_SR200,200_.jpg',
+    title: 'A Practical Guide to Personal Freedom',
+    author: 'Don Miguel Ruiz',
+  },
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg',
+    title: 'A Promised Land',
+    author: 'Barack Obama',
+  },
+];
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>
+});
+console.log(newNames);
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
-      <Book title='random title' number={22} />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 }
 const Book = (props) => {
+  const { img, title, author, } = props;
+
   return (
     <article className='book'>
-      <img src={props.img }alt='' />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img}alt='' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };

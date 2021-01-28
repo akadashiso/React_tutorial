0cdc948f950552a788576c7171f37f57359aa7ce
akadashiso <akadashiso@gmail.com>
2021-01-28 20:49:43 +0900

    Finished react basics tutorial

---
+++ src/books.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    id: 1,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/91p5b0UgbKL._AC_UL200_SR200,200_.jpg',
+    title: 'A Practical Guide to Personal Freedom',
+    author: 'Don Miguel Ruiz',
+  },
+  {
+    id: 2,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg',
+    title: 'A Promised Land',
+    author: 'Barack Obama',
+  },
+  {
+    id: 3,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/91hzcTMy3GL._AC_UL200_SR200,200_.jpg',
+    title: 'Grumpy Monkey',
+    author: 'Suzanne Lang',
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,68 +1,26 @@
-import React, { Fragment } from 'react'
-import ReactDom from 'react-dom'
+import React from 'react';
+import ReactDom from 'react-dom';
 
 // CSS
 import './index.css';
 //  setup vars
-const books = [
-  {
-    id: 1,
-    img:
-      'https://images-na.ssl-images-amazon.com/images/I/91p5b0UgbKL._AC_UL200_SR200,200_.jpg',
-    title: 'A Practical Guide to Personal Freedom',
-    author: 'Don Miguel Ruiz',
-  },
-  {
-    id: 2,
-    img:
-      'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg',
-    title: 'A Promised Land',
-    author: 'Barack Obama',
-  },
-  {
-    id: 3,
-    img:
-      'https://images-na.ssl-images-amazon.com/images/I/91hzcTMy3GL._AC_UL200_SR200,200_.jpg',
-    title: 'Grumpy Monkey',
-    author: 'Suzanne Lang',
-  },
-];
+
+import {data} from './books';
+import SpecificBook from './Book';
+import {greeting} from './testing/testing'
 
 function BookList() {
+  console.log(greeting);
   return (
   <section className="booklist">
-    {books.map((book, index) => {
-      return <Book key={book.id} {...book}></Book>;
+    {data.map((book, index) => {
+      return <SpecificBook key={book.id} {...book}></SpecificBook>;
     })}
   </section>
   );
 }
 
-const Book = ({ img, title, author }) => {
-  // attribute, eventHandler
-  // onClick, onMouseOver
-  const clickHandler = (e) => {
-    console.log(e);
-    console.log(e.target);
-    alert('はろーーーー');
-  };
-  const complexExample = (author) =>{
-    console.log(author);
-  };
-  return (
-    <article className='book'>
-      <img src={img}alt='' />
-      <h1 onClick={()=> console.log(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        reference example
-      </button>
-      <button type="button" onClick={() => complexExample(author)}>
-        more complex example
-      </button>
-    </article>
-  );
-};
+
 
 ReactDom.render(<BookList />, document.getElementById('root')
 );
